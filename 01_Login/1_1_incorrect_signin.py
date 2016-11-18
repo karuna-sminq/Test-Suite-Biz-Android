567205006da701bc630ed4ee844838ee02fb1db3
Karuna Lingham <karuna.lingham@codepandora.com>
2016-11-18 17:25:34 +0530

    Modified shell test suite script to run test files

--- 01_Login/1_1_incorrect_signin.py
+++ 01_Login/1_1_incorrect_signin.py
@@ -19,8 +19,8 @@ class CulebraTests(CulebraTestCase):
     def setUpClass(cls):
         cls.kwargs1 = {'ignoreversioncheck': False, 'verbose': False, 'ignoresecuredevice': False}
         cls.kwargs2 = {'forceviewserveruse': False, 'useuiautomatorhelper': False, 'ignoreuiautomatorkilled': True, 'autodump': False, 'startviewserver': True, 'compresseddump': True}
-        cls.options = {'start-activity': None, 'concertina': False, 'device-art': None, 'use-jar': False, 'multi-device': False, 'unit-test-class': True, 'save-screenshot': None, 'use-dictionary': False, 'glare': False, 'dictionary-keys-from': 'id', 'scale': 0.5, 'find-views-with-content-description': False, 'window': -1, 'orientation-locked': None, 'save-view-screenshots': None, 'find-views-by-id': True, 'log-actions': False, 'use-regexps': False, 'null-back-end': False, 'auto-regexps': None, 'do-not-verify-screen-dump': False, 'verbose-comments': False, 'gui': True, 'find-views-with-text': True, 'prepend-to-sys-path': False, 'drop-shadow': False, 'output': 'chartsignin1.py', 'unit-test-method': None, 'interactive': False}
-        cls.sleep = 0.01
+        cls.options = {'start-activity': None, 'concertina': False, 'device-art': None, 'use-jar': False, 'multi-device': False, 'unit-test-class': True, 'save-screenshot': None, 'use-dictionary': False, 'glare': False, 'dictionary-keys-from': 'id', 'scale': 0.5, 'find-views-with-content-description': False, 'window': -1, 'orientation-locked': None, 'save-view-screenshots': None, 'find-views-by-id': True, 'log-actions': False, 'use-regexps': False, 'null-back-end': False, 'auto-regexps': None, 'do-not-verify-screen-dump': False, 'verbose-comments': False, 'gui': True, 'find-views-with-text': True, 'prepend-to-sys-path': False, 'drop-shadow': False, 'unit-test-method': None, 'interactive': False}
+        cls.sleep = 0.85
 
     def setUp(self):
         super(CulebraTests, self).setUp()
--- sminq_tests.sh
+++ sminq_tests.sh
@@ -3,7 +3,7 @@
 echo '''
 Copyright (C) 2016 Sminq India Solutions Pvt Ltd.
 Created on 2016-10-05
-Updated on 2016-10-07
+Updated on 2016-11-18
 
  ____    __  __   ___   _   _    ___
 / ___|  |  \/  | |_ _| | \ | |  / _ \
@@ -16,7 +16,7 @@ ___) |  | |  | |  | |  | |\  | | |_| |
 '''
 
 echo "================================="
-echo "Sminq Business App v2.0.12 ..."
+echo "Sminq Business App v2.0.15 ..."
 echo "Running Test Suite v1.0.1 ..."
 echo "================================="
 
@@ -27,8 +27,27 @@ now=$d-$t
 start=`date +%s`
 
 #Run scripts
+for folder in ./*
+do
 
+  #  #Begin recording tests for every folder
+  #  adb shell screenrecord /sdcard/$folder.mp4 &
+   echo "------------------"
+   echo "$folder"
+   echo "------------------"
 
+  #Begin iterating through every file in specified folder
+  for fname in $folder/*.py
+  do
+
+    #Execute file
+    python $fname
+    total_count=$((total_count + 1))
+
+  done
+done
+
+#Test count and timer
 end=`date +%s`
 total_time=$((end-start))
 
